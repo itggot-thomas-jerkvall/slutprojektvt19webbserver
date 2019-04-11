8166f212a5213d280953cf0a569df8b2af432c30
itggot-thomas-jerkvall <38528645+itggot-thomas-jerkvall@users.noreply.github.com>
2019-04-11 10:53:15 +0200

    profile and posts

--- slutprojektvt19webbserver-master/app.rb
+++ slutprojektvt19webbserver-master/app.rb
@@ -3,10 +3,59 @@ require'sqlite3'
 require'sinatra'
 require 'byebug'
 require 'BCrypt'
-#require 'fil'
+require_relative 'fil'
 enable :sessions
-
 
 get('/') do
     slim(:index)
+end
+
+post('/login') do
+    variable = logingin(params["email"], params["password"])
+    if variable == nil
+        redirect('/')
+    else
+        session[:id] = variable
+        redirect('/lhome')      
+    end
+end
+
+get('/create') do
+    slim(:create)
+end
+
+post('/created') do
+    create_user(params["email"],params["namn"],params["password"])
+    redirect('/')
+end
+
+get('/lhome') do
+    if loggedin(session[:id]) == false
+        redirect('/')
+    else
+        session[:name] = getname(session[:id])
+        slim(:lhome)
+    end
+    slim(:lhome)
+end
+
+post('/logout') do
+    session.destroy
+    redirect('/')
+end
+
+get('/profile') do
+    posts = getposts(session[:id])
+    slim(:profile, locals:{
+        posts: posts
+    })
+end
+
+get('/cpo') do
+    slim(:cpo)
+end
+
+post('/createpost') do
+    createp(session[:id], params["text"], params["img"])
+    redirect('/cpo')
 end
---
+++ slutprojektvt19webbserver-master/fil.rb
@@ -0,0 +1,52 @@
+require'sqlite3'
+
+def create_user(email, username, password)
+    db = SQLite3::Database.new("db/reddit.db")
+    db.results_as_hash = true
+
+    hased_password = BCrypt::Password.create(password)
+
+    db.execute("INSERT INTO users(Email,Name, Password) VAlUES(?,?,?)", email, username, hased_password)
+end
+
+def loggedin(sessionid)
+    if sessionid == nil
+        return false
+    end
+    return true
+end
+
+def logingin(email, password)
+    db = SQLite3::Database.new("db/reddit.db")
+    db.results_as_hash = true
+
+    result = db.execute("SELECT Id, Name, Password FROM users WHERE users.Email = ?", email)
+    if result.length > 0 && BCrypt::Password.new(result.first["Password"]) == password
+        return result.first["Id"]
+    end
+end
+
+def getname(id)
+    db = SQLite3::Database.new("db/reddit.db")
+    db.results_as_hash = true
+    
+    result = db.execute("SELECT Name FROM users WHERE users.Id = ?", id)
+
+    return result.first["Name"]
+end
+
+def getposts(id)
+    db = SQLite3::Database.new("db/reddit.db")
+    db.results_as_hash = true
+
+    result = db.execute("SELECT Text, Image FROM posts WHERE User_Id = ?", id)
+
+    return result
+end
+
+def createp(id,text,img)
+    db = SQLite3::Database.new("db/reddit.db")
+    db.results_as_hash = true
+
+    db.execute("INSERT INTO posts(User_Id, Text, Image) VALUES(?, ?, ?)", id, text, img)
+end
